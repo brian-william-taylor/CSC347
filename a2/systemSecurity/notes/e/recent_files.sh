880a57381eb5a3038aee035b8a7c0835e2478eff
root <root@localhost.localdomain>
2017-11-18 16:36:42 -0500

    changes to recent files

--- a2/systemSecurity/notes/e/recent_files.sh
+++ a2/systemSecurity/notes/e/recent_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-find /mnt/clean -print0 | xargs -0 ls -ltd > sorted_changes_clean.txt
-find /mnt/corr -print0 | xargs -0 ls -ltd > sorted_changes_corr.txt
+find /mnt/clean -print0 | xargs -0 ls -lt > sorted_changes_clean.txt
+sed -e 's/clean//g' -i sorted_changes_clean.txt
+find /mnt/corr -print0 | xargs -0 ls -lt > sorted_changes_corr.txt
+sed -e 's/corr//g' -i sorted_changes_corr.txt
 
 diff sorted_changes_clean.txt sorted_changes_corr.txt > sorted_changes_diff.txt
